78594ad707c4f6fcce72e28048f8efea7ea9a8fb
darknessgp <darknessgp@gmail.com>
2016-07-07 11:22:20 -0500

    Correcting since escape was qoute escape

--- unrar.js
+++ unrar.js
@@ -1,15 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 var rarModule = require('node-unrar');
-var escape = function(input) {return require('shell-escape')([input])}
 
 function Unrar(config) {
 };
 
 // Method for checking if there are rar'd files to uncompress
 Unrar.prototype.HandleFolder = function(folderPath) {
-    folderPath = escape(folderPath);
-
     //figure out path to the rar file, if there is one
     var rarFile = '';
     var files = fs.readdirSync(folderPath);
@@ -21,7 +18,7 @@ Unrar.prototype.HandleFolder = function(folderPath) {
        }
     }
     // Construct the rar module with the full rar path
-    var rar = new rarModule(folderPath + rarFile);
+    var rar = new rarModule(folderPath + "/" + rarFile);
 
     rar.extract(folderPath, null, function(err) {
         if (err) {
